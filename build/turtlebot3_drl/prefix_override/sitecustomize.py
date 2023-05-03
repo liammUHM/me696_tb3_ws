c9133edfb6147edb328a5dae828cf5ab44d89577
Liam Meyer <liamm@hawaii.edu>
2023-05-02 17:14:21 -1000

    initial commit

---
+++ build/turtlebot3_drl/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/home/liam/me696_tb3_ws/install/turtlebot3_drl'
---
+++ install/holmes_nav/lib/python3.8/site-packages/holmes_nav/hh2_node.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from holmes_nav.')
+
+
+if __name__ == '__main__':
+    main()
